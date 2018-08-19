77329448973c6ae5b22ab52e4ace660b14b0019a
Daniel Kendell <daniel@kendell.org.uk>
2018-08-19 21:18:32 +0100

    move the origin to the centre and draw some guides

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background('#000000');
+
+  translate(width/2, height/2);
+  fill('#FFFF00');
+  ellipse(0,0,10,10);
+
+  stroke('#FFFFFF');
+  line(-(width/2), 0, (width/2), 0);
+  line(0, -(height/2), 0, (height/2));
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
