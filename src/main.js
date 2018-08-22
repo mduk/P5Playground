592c141015394db263c94e552ac6494b15dc7473
Daniel Kendell <daniel@kendell.org.uk>
2018-08-23 00:12:23 +0100

    added Line

--- src/main.js
+++ src/main.js
@@ -95,6 +95,26 @@ function guides() {
   line(0, -(height/2), 0, (height/2));
 }
 
+class Line {
+  constructor(begin) {
+    this.begin = begin;
+    this.colour = randomColour();
+    this.end = this.begin
+  }
+
+  draw() {
+    stroke(this.colour);
+    strokeWeight(4);
+    line(
+      this.begin.x,
+      this.begin.y,
+      this.end.x,
+      this.end.y
+    );
+  }
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // Main
 ////////////////////////////////////////////////////////////////////////////////
