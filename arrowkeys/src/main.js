859e7e7ce986e02da837b5301c757ce1b26473cb
Daniel Kendell <daniel@kendell.org.uk>
2018-09-04 13:57:55 +0100

    a scene can contain objects (Drawable)

--- arrowkeys/src/main.js
+++ arrowkeys/src/main.js
@@ -10,6 +10,8 @@ class Scene {
     this.guide_colour = '#AAAAAA';
     this.guide_weight = 1;
 
+    this.objects = [];
+
     createCanvas(this.width, this.height);
     this.updateBounds();
   }
@@ -26,6 +28,20 @@ class Scene {
     this.maxy =  (this.height / 2) + this.gutter_px;
   }
 
+  addObject(object) {
+    this.objects.push(object);
+  }
+
+  updateObjects() {
+    this.objects.forEach((o) => {
+      if (!(object instanceof Drawable)) {
+        console.log("Object is not drawable!", object);
+        return;
+      }
+      object.update(this);
+    });
+  }
+
   drawGuides() {
     stroke(this.guide_colour);
     strokeWeight(this.guide_weight);
@@ -33,14 +49,29 @@ class Scene {
     line(0, -(this.height/2), 0, (this.height/2));
   }
 
+  drawObjects() {
+    this.objects.forEach((o) => {
+      if (!(object instanceof Drawable)) {
+        console.log("Object is not drawable!", object);
+        return;
+      }
+      object.draw();
+    });
+  }
+
   draw() {
     background(this.background);
 
     translate(this.width/2, this.height/2);
 
+    this.updateObjects();
+
     if (this.draw_guides) {
       this.drawGuides();
     }
+
+    this.drawObjects();
+
   }
 }
 
